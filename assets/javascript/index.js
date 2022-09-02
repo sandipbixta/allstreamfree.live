f5b6870def4bbf0abb8fe372549aa19c619c5eea
Abdulaziz Abdullahi <abdulaziz.abdullahi@student.moringaschool.com>
2022-09-02 15:27:06 +0300

    add code to prevent default behavior  of the news section anchor tags

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -26,10 +26,15 @@ function submit(){
         fetch('http://localhost:3000/formData',fetchObj).then(res=>res.json()).then(data=>console.log(data));
         let p = document.createElement('p');
         document.getElementById('bottom').append(p);
-        p.style.fontFamily="'Inter', sans-serif";
+        p.style.fontFamily="'Poppins', sans-serif";
         p.style.fontSize="40px"
         p.innerText="Thank you for subscribing. We have received your data."
         form.remove();
         // form.reset();
     });
 }
+
+let a = document.querySelectorAll('.news a')
+a[0].addEventListener('click',e=>e.preventDefault());
+a[1].addEventListener('click',e=>e.preventDefault());
+a[2].addEventListener('click',e=>e.preventDefault());
