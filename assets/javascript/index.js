f215b14db734510b8e9b8b8b3319f042a82229d2
Abdulaziz Abdullahi <abdulaziz.abdullahi@student.moringaschool.com>
2022-09-02 12:59:32 +0300

    give the form on the footer its functionality

---
+++ assets/javascript/index.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded',submit);
+
+
+function submit(){
+    let form = document.querySelector('#form');
+    form.addEventListener('submit',function(e){
+        e.preventDefault();
+        let fname=document.getElementById('fname').value;
+        let lname=document.getElementById('lname').value;
+        let email=document.getElementById('email1').value;
+
+        let formData= {
+            fname:fname,
+            lname:lname,
+            email:email
+        }
+        let fetchObj={
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+              },
+              body: JSON.stringify(formData),
+    
+            };
+        fetch('http://localhost:3000/formData',fetchObj).then(res=>res.json()).then(data=>console.log(data));
+        let p = document.createElement('p');
+        document.getElementById('bottom').append(p);
+        p.style.fontFamily="'Inter', sans-serif";
+        p.style.fontSize="40px"
+        p.innerText="Thank you for subscribing. We have received your data."
+        form.remove();
+        // form.reset();
+    });
+}
